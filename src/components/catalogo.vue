00abc29d30ff27a24aaa3a52dc63d2aad9c7e825
elog1011 <ortizelizabeth451@gmail.com>
2021-12-07 17:04:30 -0500

    Optimizando Vistas 07 Diciembre

--- src/components/catalogo.vue
+++ src/components/catalogo.vue
@@ -1,40 +1,123 @@
 <template>
   <div class="information">
-    <h1>
-      ¡Catalogo
-      <span>{{ catalogoDetailById.catalogoId }}</span
-      >!
-    </h1>
-    <div class="details">
-      <h3>Hotel </h3>
-
-        <h2>
-        Nombre:
-        <span>{{ catalogoDetailById.Nombre }}</span>
-        </h2>
       
-        <h2>
-        Ubicacion:
-        <span>{{ catalogoDetailById.Ubicacion }}</span>
-        </h2>  
-
-        <h2>
-        Calificación:
-        <span>{{ catalogoDetailById.Calificacion }}</span>
-        </h2>
-        
-       <h2>   
-        Descripción:
-        <span>{{ catalogoDetailById.Descripcion }}</span>
-        </h2>  
-      
-         <h2>  
-        Correo:
-        <span>{{ catalogoDetailById.Correo }}</span>
-        </h2>  
-
+    <div class="create-route">
+      <h1>Nuestros Hoteles Asociados</h1>
     </div>
+
+  
+      <div class="cuadro_contenedor">
+        <div class="create-route">
+          Nombre:<br>
+          <span>{{ catalogoDetailById.Nombre }}</span>
+        </div>
+        <br />
+
+        <div class="create-route">
+          Ubicación:
+        <span>{{ catalogoDetailById.Ubicacion }}</span>
+        </div>
+        <br />
+
+        <div class="create-route">
+          Calificación:
+        <span>{{ catalogoDetailById.Calificacion }}</span>
+        </div>
+        <br />
+
+        <div class="create-route">
+          Descripción:
+        <span>{{ catalogoDetailById.Descripcion }}</span>
+        </div>
+        <br />
+
+        <div class="create-route">
+          Correo:
+          <span>{{ catalogoDetailById.Correo }}</span>
+        </div>
+        <br />
+
+        <nav>
+        <button v-if="!is_auth" v-on:click="reservas">Reservar</button>
+        </nav>
+      </div>
+        
+      <div class="cuadro_contenedor">
+        <div class="create-route">
+          Nombre:
+        <span>{{ catalogoDetailById.Nombre }}</span>
+        </div>
+        <br />
+
+        <div class="create-route">
+          Ubicación:
+        <span>{{ catalogoDetailById.Ubicacion }}</span>
+        </div>
+        <br />
+
+        <div class="create-route">
+          Calificación:
+        <span>{{ catalogoDetailById.Calificacion }}</span>
+        </div>
+        <br />
+
+        <div class="create-route">
+          Descripción:
+          <span>{{ catalogoDetailById.Descripcion }}</span>
+        </div>
+        <br />
+
+        <div class="create-route">
+          Correo:
+        <span>{{ catalogoDetailById.Correo }}</span>
+        </div>
+        <br />
+
+        <nav>
+        <button v-if="!is_auth" v-on:click="reservas">Reservar</button>
+        </nav>
+      </div>
+
+      <div class="cuadro_contenedor">
+        <div class="create-route">
+          Nombre:
+        <span>{{ catalogoDetailById.Nombre }}</span>
+        </div>
+        <br />
+
+        <div class="create-route">
+          Ubicación:
+        <span>{{ catalogoDetailById.Ubicacion }}</span>
+        </div>
+        <br />
+
+        <div class="create-route">
+          Calificación:
+        <span>{{ catalogoDetailById.Calificacion }}</span>
+        </div>
+        <br />
+
+        <div class="create-route">
+          Descripción:
+        <span>{{ catalogoDetailById.Descripcion }}</span>
+        </div>
+        <br />
+
+        <div class="create-route">
+          Correo:
+        <span>{{ catalogoDetailById.Correo }}</span>
+        </div>
+        <br />
+
+        <nav>
+        <button v-if="!is_auth" v-on:click="reservas">Reservar</button>
+        </nav>
+        <br>
+      </div>
+        
   </div>
+  
+
 </template>
 
 <script>
@@ -44,7 +127,13 @@ export default {
   name: "catalogo",
   data: function () {
     return {
-      catalogoId: jwt_decode(localStorage.getItem("token_refresh")).catalogo_Id,
+      catalogoId: jwt_decode(localStorage.getItem("token_refresh")).catalogo_id, 
+      
+      catalogoDetailById: {
+        Nombre: "",
+        Correo: "",
+      }
+      /*catalogoId: 1,
       catalogoDetailById: {
         Nombre: "",
         Ubicacion: "",
@@ -52,19 +141,27 @@ export default {
         Direccion: "",
         Descripcion: "",
         Correo: "",
-        
-        
-      },
+
+        catalogoId: 2,
+        catalogoDetailById: {
+          Nombre: "",
+          Ubicacion: "",
+          Calificacion: 0,
+          Direccion: "",
+          Descripcion: "",
+          Correo: "",
+        },
+      },*/
     };
   },
   apollo: {
     catalogoDetailById: {
       query: gql`
-        query ($catalogoId: Int!) {
+        query Query($catalogoId: Int!) {
           catalogoDetailById(catalogoId: $catalogoId) {
             Nombre
-            Ubicacion
             Calificacion
+            Ubicacion
             Direccion
             Descripcion
             Correo
@@ -82,13 +179,33 @@ export default {
 </script>
 
 <style>
+.cuadro_contenedor {
+  padding: 10px;
+  margin-bottom: 10px;
+  background-color: rgb(245, 245, 245);
+  border: solid 1px red;
+  display: grid;
+  grid-template-columns: 33% 33% 33%;
+  border-radius: 10px;
+  box-shadow: 3px 3px 3px 2px rgba(197, 23, 23, 0.2);
+  width: 50%;
+  height: auto;
+}
+
+.create-route {
+  border-radius: 5px;
+  text-align: left;
+  margin: 1px;
+  padding: 0px;
+  font-family: Verdana, Geneva, Tahoma, sans-serif;
+}
 .information {
   margin: 0;
   padding: 0%;
-  width: 100%;
-  height: 100%;
+  width: 90%;
+  height: 90%;
   display: flex;
-  flex-direction: column;
+  flex-direction: center;
   justify-content: center;
   align-items: center;
 }
@@ -102,7 +219,7 @@ export default {
 }
 .information span {
   color: crimson;
-  font-weight: bold;
+  font-family: Verdana, Geneva, Tahoma, sans-serif;
 }
 .details h3 {
   font-size: 35px;
@@ -114,9 +231,13 @@ export default {
   color: #283747;
 }
 .details {
-  border: 3px solid rgba(0, 0, 0, 0.3);
-  border-radius: 20px;
-  padding: 30px 80px;
-  margin: 30px 0 0 0;
+  border: 3px solid #283747;
+  border-radius: 10px;
+  width: 80%;
+  height: 80%;
+  display: flex;
+  flex-direction: column;
+  justify-content: right;
+  align-items: right;
 }
 </style>
